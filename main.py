b6c5f122487abf35c91411df85484f99e1ef8f42
12345notfound <yatchenkokv@mail.ru>
2022-12-19 17:28:00 +0500

    класс пули и ящика

--- main.py
+++ main.py
@@ -3,22 +3,72 @@ import random
 
 
 class LootBox(pygame.sprite.Sprite):
+    """Класс ящика с улучшениями"""
     def __init__(self, x, y):
-        super().__init__()
-        self.surface = pygame.surface.Surface((50, 50), masks=(10, 10, 10))
+        super().__init__(all_sprites)
+        self.image = pygame.surface.Surface((50, 50))
+        self.image.fill((192, 0, 192))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
 
 
-running = True
+class Bullet(pygame.sprite.Sprite):
+    """Класс стены"""
+    def __init__(self, x, y, speed_x, speed_y, damage):
+        super().__init__(all_sprites)
+        self.image = pygame.surface.Surface((20, 20))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = y
+        # float coords for better (?) movement
+        self.float_x = x
+        self.float_y = y
+        self.speedx = speed_x  # скорость по х и у
+        self.speedy = speed_y
+        self.damage = damage  # урон
 
-while running:
-    # внутри игрового цикла ещё один цикл
-    # приёма и обработки сообщений
-    for event in pygame.event.get():
-        # при закрытии окна
-        if event.type == pygame.QUIT:
-            running = False
-        # РЕАКЦИЯ НА ОСТАЛЬНЫЕ СОБЫТИЯ
-        # ...
-    # отрисовка и изменение свойств объектов
-    # ...
-    pygame.display.flip()
+    def update(self):
+        self.float_x += self.speedx
+        self.float_y += self.speedy
+        self.rect.centerx = int(self.float_x)
+        self.rect.centery = int(self.float_y)
+        # print(self.float_x, self.float_y)
+        # print()
+
+
+# class Wall(pygame.sprite.Sprite):
+#     """Класс стены"""
+#     def __init__(self, x, y, ):
+
+
+if __name__ == '__main__':
+    FPS = 30
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    walls = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    running = True
+    # test
+    Bullet(10, 10, 1.4, 3.8, damage=10)
+    LootBox(30, 30)
+
+    while running:
+        # внутри игрового цикла ещё один цикл
+        # приёма и обработки сообщений
+        for event in pygame.event.get():
+            # при закрытии окна
+            if event.type == pygame.QUIT:
+                running = False
+            # РЕАКЦИЯ НА ОСТАЛЬНЫЕ СОБЫТИЯ
+        # отрисовка и изменение свойств объектов
+        all_sprites.update()
+        screen.fill('black')
+        all_sprites.draw(screen)
+        clock.tick(FPS)
+        pygame.display.flip()
+    pygame.quit()
